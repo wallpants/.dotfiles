4c39d27b4555100513fc1bf2b93bfec6d0658b08
Gual Casas <gual.casas@gmail.com>
2023-08-05 18:00:36 -0600

    update os specific

--- zsh/linux.zsh
+++ zsh/linux.zsh
@@ -5,3 +5,9 @@ export PATH="$PATH:$ANDROID_HOME/platform-tools"
 export DOCKER_HOST=unix:///home/gualcasas/.docker/desktop/docker.sock
 
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+echo ""
+echo ""
+echo "Microsoft(R) Windows 95"
+echo "   (C)Copyright Fedora Corp 1981-1996."
+echo ""
--- zsh/mac.zsh
+++ zsh/mac.zsh
@@ -1,2 +1,8 @@
 # brew
 export PATH="/usr/local/sbin:$PATH"
+
+echo ""
+echo ""
+echo "Microsoft(R) Windows 95"
+echo "   (C)Copyright Darwin Corp 1981-1996."
+echo ""
