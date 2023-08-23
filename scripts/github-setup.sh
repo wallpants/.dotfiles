ebe4b3450d49f6be211da723d0b16d310f663514
Gual Casas <gual.casas@gmail.com>
2023-08-23 16:46:27 -0600

    move scripts into scripts dir

---
+++ scripts/github-setup.sh
@@ -0,0 +1,17 @@
+EMAIL=""
+GIT_USERNAME=""
+
+if [ ! -e ~/.ssh/id_ed25519.pub ]; then
+# Check if the vars are empty
+if [[ -z "$EMAIL"  || -z "$GIT_USERNAME" ]]; then
+  echo "EMAIL or USERNAME not set"
+  exit 1
+fi
+
+  git config --global user.name "$GIT_USERNAME"
+  git config --global user.email "$EMAIL"
+
+  ssh-keygen -t ed25519 -C "$EMAIL"
+
+  cat ~/.ssh/id_ed25519.pub
+fi
