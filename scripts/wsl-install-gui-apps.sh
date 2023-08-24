abe8ebc709342f706248285a6d74764aec0181cc
Gual Casas <gual.casas@gmail.com>
2023-08-23 18:24:57 -0600

    install kitty wsl

--- scripts/wsl-install-gui-apps.sh
+++ scripts/wsl-install-gui-apps.sh
@@ -11,3 +11,6 @@ sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt install --fix-broken -y
 sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+# kitty
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
