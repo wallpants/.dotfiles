66cc137bc2578676314c976acee3b57b36bfa8d7
Gual Casas <gual.casas@gmail.com>
2023-08-29 23:56:54 -0600

    fix pattern, "github-preview" dir was being ignored

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -9,7 +9,7 @@ return {
     version = false,
     opts = {
       defaults = {
-        file_ignore_patterns = { ".git", "dist", "pnpm-lock.yaml" },
+        file_ignore_patterns = { ".git/", "dist", "pnpm-lock.yaml" },
         path_display = {
           truncate = 10, -- number: padding to the right
         },
