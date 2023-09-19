a0c0e77f911b18e09ca0d8c36980c9b251410d57
Gual Casas <gual.casas@gmail.com>
2023-09-19 01:42:05 -0600

    update bunvim

--- nvim/lua/gual/bunvim.lua
+++ nvim/lua/gual/bunvim.lua
@@ -1,12 +1,21 @@
 local Utils = require("gual.utils")
 
 local function func1()
+    vim.print("starting")
+
     vim.fn.jobstart("bun run src/tests.ts", {
         cwd = vim.fn.expand("~/Projects/nvim-plugins/bunvim/"),
+        on_stderr = function(_, data)
+            vim.print(data)
+        end,
+        on_stdout = function(_, data)
+            vim.print(data)
+        end,
     })
 end
 
 local function func2()
+    vim.print("notifying")
     vim.rpcnotify(0, "something")
 end
 
