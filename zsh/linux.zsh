56154cbaf19f0b8f17785dac8393c1a31d1fc9e8
wallpants <47203170+wallpants@users.noreply.github.com>
2023-11-16 12:11:54 -0600

    load terraform autocomplete

--- zsh/linux.zsh
+++ zsh/linux.zsh
@@ -9,3 +9,7 @@ FPATH="$BREW_PREFIX/share/zsh/site-functions:${FPATH}" # load brew completions
 export NVM_DIR="$HOME/.nvm"
 [ -s "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh" ] && \. "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh"                                       # This loads nvm
 [ -s "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
+
+# load terraform autocomplete
+autoload -U +X bashcompinit && bashcompinit
+complete -o nospace -C /home/linuxbrew/.linuxbrew/Cellar/terraform/1.6.4/bin/terraform terraform
