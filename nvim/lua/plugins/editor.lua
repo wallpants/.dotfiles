21bda7e0dee96607a1408e15c46977c611c0805e
wallpants <47203170+wallpants@users.noreply.github.com>
2024-05-04 14:13:53 -0600

    debug github-preview

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -36,7 +36,7 @@ return {
             theme = {
                 high_contrast = true,
             },
-            -- log_level = "debug",
+            log_level = "debug",
         },
         config = function(_, opts)
             local gpreview = require("github-preview")
