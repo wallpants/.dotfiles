12500d2da462092d9b0de613cde3158b943fc589
wallpants <47203170+wallpants@users.noreply.github.com>
2024-05-18 18:34:06 -0600

    feat(android): setup android mac

--- zsh/mac.zsh
+++ zsh/mac.zsh
@@ -4,3 +4,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
 alias pip="pip3"
+
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/platform-tools
