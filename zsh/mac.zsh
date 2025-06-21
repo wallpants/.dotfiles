4f8051ca977c812d2732115ad8ffd1741a026abd
wallpants <47203170+wallpants@users.noreply.github.com>
2025-06-21 10:54:13 -0600

    update zshrc

--- zsh/mac.zsh
+++ zsh/mac.zsh
@@ -9,3 +9,9 @@ export JAVA_HOME=/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home
 export ANDROID_HOME=$HOME/Library/Android/sdk
 export PATH=$PATH:$ANDROID_HOME/emulator
 export PATH=$PATH:$ANDROID_HOME/platform-tools
+
+# Add postgresql@16 to PATH
+export PATH="/opt/homebrew/opt/postgresql@16/bin:$PATH"
+
+# Added by LM Studio CLI (lms)
+export PATH="$PATH:/Users/wallpants/.lmstudio/bin"
