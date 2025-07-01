912acd1abeca5559565ce184ec1df11defd72284
wallpants <47203170+wallpants@users.noreply.github.com>
2025-06-30 19:51:15 -0600

    test clipboard

--- nvim/lua/gual/options.lua
+++ nvim/lua/gual/options.lua
@@ -60,3 +60,26 @@ opt.winborder = "single" -- single, rounded
 vim.g.markdown_recommended_style = 0
 
 vim.g.sql_type_default = "sqlanywhere"
+--
+-- This clipboard stuff is required to get rid of the error messages
+-- when ssh'ing to a machine
+vim.o.clipboard = "unnamedplus"
+
+local function paste()
+    return {
+        vim.fn.split(vim.fn.getreg(""), "\n"),
+        vim.fn.getregtype(""),
+    }
+end
+
+vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+        ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+        ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+        ["+"] = paste,
+        ["*"] = paste,
+    },
+}
