6bf5308dafed61aca39a374d16aea4f45f734d28
wallpants <47203170+wallpants@users.noreply.github.com>
2026-06-06 16:51:10 -0600

    add image support to neovim

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -3,6 +3,13 @@ local Utils = require("gual.utils")
 ---@type LazyPluginSpec[]
 return {
     {
+        "3rd/image.nvim",
+        build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+        opts = {
+            processor = "magick_cli",
+        },
+    },
+    {
         "nvim-tree/nvim-web-devicons",
         opts = {
             strict = true,
